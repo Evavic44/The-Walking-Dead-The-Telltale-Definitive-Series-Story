caba06a63ce2e36e4726a8ab7d3c7d41224884fd
Eke Victor <evavic44@gmail.com>
2021-03-05 18:55:08 +0100

    Implemented Smooth Scrolling

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Smooth Scrolling
+$('a, .btn').on('click', function (e) {
+    if (this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 0,
+            },
+            800
+            );
+        }
+    });
